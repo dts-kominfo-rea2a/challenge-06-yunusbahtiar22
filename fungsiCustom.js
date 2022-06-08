94a4df01c377ef556face67536f1e0858f348e6e
yunnix22 <aboomarmoshab22@gmail.com>
2022-06-08 09:06:31 +0700

    Add new functionality

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
+const { parse } = require('path');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,32 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  // asumsikan file-file diatas sudah diketahui strukturnya
+
+  const result = [];
+
+  fs.readFile(file1, 'utf-8', (err, data) => {
+    if (err) return fnCallback(err, null);
+
+    const parsed = JSON.parse(data);
+    result.push(parsed['message'].split(' ')[1]);
+
+    fs.readFile(file2, 'utf-8', (err, data) => {
+      if (err) return fnCallback(err, null);
+      
+      const parsed = JSON.parse(data);
+      result.push(parsed[0].message.split(' ')[1]);
+      
+      fs.readFile(file3, 'utf-8', (err, data) => {
+        const parsed = JSON.parse(data);
+        result.push(parsed[0]['data']['message'].split(' ')[1])
+
+        return fnCallback(null, result);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
